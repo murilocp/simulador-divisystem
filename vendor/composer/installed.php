1ac06e003f386e2a1d13286d614c994f3ac6b461
Murilo Picinato <murilo@sysplan.com.br>
2021-07-22 22:39:46 -0300

    vai porra

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1322601e07fb4942ec4ecc54a4fa2d25d9eedf36',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1322601e07fb4942ec4ecc54a4fa2d25d9eedf36',
+            'dev_requirement' => false,
+        ),
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v195',
+            'version' => '195.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'reference' => '88cf8f624299856ac2ef3c096eaf93a4eb4c94a4',
+            'dev_requirement' => true,
+        ),
+    ),
+);
